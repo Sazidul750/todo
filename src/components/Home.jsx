94baae27d4aeb64e28985c917b0e5776aa61f792
sazidul7501~ <sazidulok69@gmail.com>
2023-07-05 21:09:00 -1200

    done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Todos from './Todos'
 import NewTodos from './NewTodos'
 
 export default function Home() {
-    const dummytodos=[
-        {
-            id:"1",
-            title:"this frist title ",
-            description:"this is description"
-        },
-        {
-            id:"1",
-            title:"this frist title ",
-            description:"this is description"
-        }
-    ]
+    const [todos,setTodos]=useState([])
+   
   return (
     <div>
         <NewTodos />
-        <Todos todos={dummytodos} />
+        <Todos todos={todos} />
 
     </div>
   )
